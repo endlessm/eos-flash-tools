78fb0332db08044ab1a53be00840dc223a541cf9
Roberto Goizueta <goizueta@endlessm.com>
2015-01-13 17:09:08 -0800

    usb-reflash: added factory test
    Added the factory test that will be detected by eos-factory-test.
    It contains the reflashing initramfs and the start.sh file that
    unpacks that initramfs into /run/initramfs.
    
    [endlessm/eos-shell#4001]

---
+++ eos-usb-reflash/factory-test/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This is a factory test script that places a barebones system in
+# /run/intramfs. This fs will mount the USB disk and flash its 
+# image to internal storage.
+
+set -e
+
+KERNEL_VERSION="$(uname -r)"
+
+echo "Unpacking initramfs in /run/initramfs"
+mkdir /run/initramfs
+cd /run/initramfs
+INITRAMFS_PATH="/var/wistron/initramfs-${KERNEL_VERSION}.img"
+
+if [ -f $INITRAMFS_PATH ] ; then
+    zcat $INITRAMFS_PATH | cpio -id >/dev/null || exit 1
+    echo "Successfully unpacked initramfs."
+else
+    echo "${INITRAMFS_PATH} could not be found. Use backup flashing method."
+    sleep 10
+    exit 1
+fi
+sleep 2
+
+echo "Removing testsuite directory and powering off."
+sleep 2
+rm -rf /var/wistron
+poweroff
