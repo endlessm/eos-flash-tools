3c4e09b6db8475e5e7592fb4913277f1c5e565fd
João Paulo Rechi Vita <jprvita@endlessm.com>
2021-03-08 15:50:47 -0800

    start.sh: Try to load the in-kernel exfat driver if needed
    Prior to EOS 4.0 exFAT support was provided via FUSE. Since the initrd
    does not contain the FUSE driver or the kernel module, it will not be
    possible to mount the USB device containing the image once we pivot to
    the initrd.
    
    One way around this problem is to manually load the in-kernel exfat
    driver before pivot-root, so it is available in the running kernel after
    pivot-root (which is also how this works for any other filesystem we
    support in the kernel).
    
    This commit tried to load the in-kernel exfat driver if it detects any
    exFAT filesystem attached to the system. This is the best we can do from
    start.sh, as at the point it is running it has already been copied from
    the USB device to /var by eos-factory-test-helper.service.
    
    https://phabricator.endlessm.com/T31536

--- eos-usb-reflash/factory-test/start.sh
+++ eos-usb-reflash/factory-test/start.sh
@@ -6,6 +6,26 @@
 set -e
 mydir=$(dirname $(readlink -f $0))
 
+# If this script is running from an exFAT filesystem we need to manually load
+# the exfat kernel module, as on EOS prior to 4.0 the USB device has been
+# mounted using the FUSE exFAT driver, and the initramfs we are going to pivot
+# to does not contain an exFAT driver (neither in-kernel nor via FUSE).
+FSTYPES=$(lsblk --noheadings --output FSTYPE)
+if [[ "$FSTYPES" =~ "exfat" ]] ; then
+    echo "Detected an exFAT filesystem attached to this machine;"
+    echo "Trying to load the 'exfat' kernel module..."
+    if ! modprobe exfat ; then
+        echo "This version of EOS does not have in-kernel exFAT support!"
+        echo "If you are trying to reflash this computer from a USB device "
+        echo "formatted as exFAT, please try again with a device formatted "
+        echo "with a compatible filesystem (ext2 / ext4)."
+        echo "Removing testsuite directory and powering off in 30 seconds..."
+        sleep 30
+        rm -rf $mydir
+        poweroff
+    fi
+fi
+
 echo "Unpacking initramfs in /run/initramfs"
 
 if [ ! -d /run/initramfs ] ; then
